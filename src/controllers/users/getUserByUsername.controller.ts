4ede0430d2d9bbc27a930e1eb960a1f21544f300
Asciente-rks <sonioralphkenneth@gmail.com>
2026-02-12 15:58:07 +0800

    Added getUserByEmail and getUserByUsername token authentication

--- src/controllers/users/getUserByUsername.controller.ts
+++ src/controllers/users/getUserByUsername.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { UserRepository } from '../../repositories/users/user.repository';
 import { UserService } from '../../services/users/user.service';
+import { AuthRequest } from '../../middlewares/auth.middleware';
 
 const userRepository = new UserRepository();
 const userService = new UserService(userRepository);
@@ -8,6 +9,12 @@ const userService = new UserService(userRepository);
 export const getUserByUsername = async (req: Request, res: Response) => {
   try {
     const username = req.params.username as string;
+    const authUsername = (req as AuthRequest).user?.username;
+
+    if (username !== authUsername) {
+      return res.status(403).json({ error: 'Forbidden: You can only view your own profile' });
+    }
+
     const user = await userService.findByUsername(username);
 
     if (!user) return res.status(404).json({ error: 'User not found' });

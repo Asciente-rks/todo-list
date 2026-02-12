e656766ef8cded5414cf24528bff213b232ecace
Asciente-rks <sonioralphkenneth@gmail.com>
2026-02-12 15:04:45 +0800

    Fixed bug: userId from todo and id from users table will now match

--- src/controllers/todo/createTodo.controller.ts
+++ src/controllers/todo/createTodo.controller.ts
@@ -1,17 +1,20 @@
 import { Request, Response } from 'express';
-import { TodoService } from '../../services/todo/todo.service';
 import { TodoRepository } from '../../repositories/todo/todo.repository';
+import { TodoService } from '../../services/todo/todo.service';
 import { AuthRequest } from '../../middlewares/auth.middleware';
 
-const todoService = new TodoService(new TodoRepository());
+const todoRepository = new TodoRepository();
+const todoService = new TodoService(todoRepository);
 
 export const createTodo = async (req: Request, res: Response) => {
   try {
     const userId = (req as AuthRequest).user!.id;
     const todoData = req.body;
-    const createdTodo = await todoService.createTodo(userId, todoData);
-    return res.status(201).json(createdTodo);
+
+    const todo = await todoService.createTodo(userId, todoData);
+    
+    res.status(201).json(todo);
   } catch (error: any) {
-    return res.status(500).json({ message: error.message });
+    res.status(400).json({ message: error.message });
   }
 };
